617e58792290e8040b44d1dd8f846eae07f37302
Dag Østgulen Heradstveit <dag.heradstveit@nsd.uib.no>
2015-01-13 10:34:24 +0100

    More

--- src/main/java/no/nsd/qddt/exception/UserNotFoundException.java
+++ src/main/java/no/nsd/qddt/exception/UserNotFoundException.java
@@ -1,12 +1,17 @@
 package no.nsd.qddt.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException{
 
+    private static final Logger logger = LoggerFactory.getLogger(UserNotFoundException.class);
+
     public UserNotFoundException(String email) {
         super("Could not find User by email '" + email + "'.");
+        logger.error("Could not find user by email " + email + ".");
     }
 }
