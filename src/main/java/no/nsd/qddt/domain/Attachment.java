3fed3e7e249cd0cdef9e916405578ad69174342a
SNO <stig.norland@nsd.uib.no>
2015-01-19 13:29:08 +0100

    første version

---
+++ src/main/java/no/nsd/qddt/domain/Attachment.java
@@ -0,0 +1,157 @@
+package no.nsd.qddt.domain;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * @author Stig Norland
+ */
+@Entity
+@Table(name = "Attachment")
+public class Attachment  {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID guid;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @Type(type = "org.jadira.usertype.dateandtime.threeten.PersistentLocalDateTime")
+    @Column(name = "created")
+    private LocalDateTime created;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User createdBy;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "change_id")
+    private ChangeReason changeReason;
+
+    private String changeComment;
+
+    @ManyToOne
+    @JoinColumn(name="module_id")
+    private Module module;
+
+    private String path;
+
+    public UUID getGuid() {
+        return guid;
+    }
+
+    public void setGuid(UUID guid) {
+        this.guid = guid;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public User getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(User createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ChangeReason getChangeReason() {
+        return changeReason;
+    }
+
+    public void setChangeReason(ChangeReason changeReason) {
+        this.changeReason = changeReason;
+    }
+
+    public String getChangeComment() {
+        return changeComment;
+    }
+
+    public void setChangeComment(String changeComment) {
+        this.changeComment = changeComment;
+    }
+
+    public Module getModule() {
+        return module;
+    }
+
+    public void setModule(Module module) {
+        this.module = module;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Attachment that = (Attachment) o;
+
+        if (changeComment != null ? !changeComment.equals(that.changeComment) : that.changeComment != null)
+            return false;
+        if (changeReason != null ? !changeReason.equals(that.changeReason) : that.changeReason != null) return false;
+        if (created != null ? !created.equals(that.created) : that.created != null) return false;
+        if (createdBy != null ? !createdBy.equals(that.createdBy) : that.createdBy != null) return false;
+        if (guid != null ? !guid.equals(that.guid) : that.guid != null) return false;
+        if (module != null ? !module.equals(that.module) : that.module != null) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (path != null ? !path.equals(that.path) : that.path != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = guid != null ? guid.hashCode() : 0;
+        result = 31 * result + (created != null ? created.hashCode() : 0);
+        result = 31 * result + (createdBy != null ? createdBy.hashCode() : 0);
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (changeReason != null ? changeReason.hashCode() : 0);
+        result = 31 * result + (changeComment != null ? changeComment.hashCode() : 0);
+        result = 31 * result + (module != null ? module.hashCode() : 0);
+        result = 31 * result + (path != null ? path.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Attachment{" +
+                "guid=" + guid +
+                ", created=" + created +
+                ", createdBy=" + createdBy +
+                ", name='" + name + '\'' +
+                ", changeReason=" + changeReason +
+                ", changeComment='" + changeComment + '\'' +
+                ", module=" + module +
+                ", path='" + path + '\'' +
+                '}';
+    }
+}
