1b85c95c608a4c051467bcc8d3337389b5d5087f
scav <dagherad@gmail.com>
2015-01-30 16:54:18 +0100

    Removed interface we do not need. EnversRevisionRepository includes them all

--- src/main/java/no/nsd/qddt/repository/SurveyRepository.java
+++ src/main/java/no/nsd/qddt/repository/SurveyRepository.java
@@ -2,15 +2,12 @@ package no.nsd.qddt.repository;
 
 import no.nsd.qddt.domain.Survey;
 import org.springframework.data.envers.repository.support.EnversRevisionRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.history.RevisionRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface SurveyRepository extends RevisionRepository<Survey, Long, Integer>, JpaRepository<Survey, Long>,
-        EnversRevisionRepository<Survey, Long, Integer> {
+public interface SurveyRepository extends EnversRevisionRepository<Survey, Long, Integer> {
 
     Optional<Survey> findById(Long id);
 }
