bdf7bb9bcb22653a6862b4d22927dc6dee5e1428
Dag Østgulen Heradstveit <dag.heradstveit@nsd.uib.no>
2015-10-05 15:05:11 +0200

    Added fix for comments not adding correctly

--- src/test/java/service/CommentServiceTest.java
+++ src/test/java/service/CommentServiceTest.java
@@ -8,14 +8,12 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 
 
 /**
@@ -29,38 +27,23 @@ public class CommentServiceTest {
     private CommentService commentService;
 
     @Test
-    @Transactional
     public void saveCommentTest() throws Exception {
+        UUID ownerId = UUID.randomUUID();
 
-        Comment parent = new Comment("PARENT");
-        parent.setOwnerId(UUID.fromString("2fed9aab-748e-48d2-ac7f-57c076029a0b"));
-        Comment firstChild = new Comment("CHILD-ONE");
-            firstChild.setOwnerId(UUID.fromString("2fed9aab-748e-48d2-ac7f-57c076029a0b"));
-            firstChild.addComment(new Comment("CHILD OF CHILD-ONE"));
-        parent.addComment(firstChild);
-        parent.addComment(new Comment("CHILD-TWO"));
-        parent.addComment(new Comment("CHILD-THREE"));
-        commentService.save(parent);
+        Comment parent = commentService.save(new Comment("PARENT"));
+        parent.setOwnerId(ownerId);
+
+        Comment saved = commentService.save(new Comment("CHILD-ONE"));
+        parent.addComment(saved);
+        saved.addComment(commentService.save(new Comment("CHILD OF CHILD-ONE")));
+        parent.addComment(commentService.save(new Comment("CHILD-TWO")));
+        parent.addComment(commentService.save(new Comment("CHILD-THREE")));
+        parent = commentService.save(parent);
 
         Comment root  = commentService.findOne(parent.getId());
 
-        assertEquals("Her skal vi ha 5 stk...",is(5L), parent.treeSize());
-        assertEquals("Her skal vi ha 3 stk...", is(3L), parent.getChildren().size());
-        assertThat("Parent må være root element",parent, is(root));
+        assertThat("Parent tree should have 5 elements.",  parent.treeSize(), is(5));
+        assertThat("Parent should have 3 children.", parent.getChildren().size(), is(3));
+        assertEquals("Parent and root should be equal.", parent, root);
     }
-
-
-    /**
-     * Tests for the child -> parent relation are not
-     * neccessary, as a child will never be populated
-     * without the context of its parent.
-     *
-     * Children will always be added to the parent.
-     * When listing comments, they do not need to have an
-     * explicit relation to parents.
-     *
-     * Children will be added to the parent, or a child
-     * will have a child added to it, making it a parent
-     * and a child at the same time.
-     */
 }
