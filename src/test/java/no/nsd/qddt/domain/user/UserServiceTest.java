0b0b46e0e61321c8451eb0279e906614073d8e23
SNO <stig.norland@nsd.uib.no>
2015-10-07 16:17:27 +0200

    changekind auto set value

--- src/test/java/no/nsd/qddt/domain/user/UserServiceTest.java
+++ src/test/java/no/nsd/qddt/domain/user/UserServiceTest.java
@@ -1,14 +1,25 @@
 package no.nsd.qddt.domain.user;
 
 import no.nsd.qddt.domain.AbstractServiceTest;
+import no.nsd.qddt.exception.ResourceNotFoundException;
 import no.nsd.qddt.exception.UserNotFoundException;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
 
 public class UserServiceTest extends AbstractServiceTest {
 
     @Autowired
     private UserService userService;
+
+//    @Autowired
+//    private UserRepository userRepository;
 
     @Test
     public void testFindUserByEmail() throws Exception {
@@ -19,5 +30,109 @@ public class UserServiceTest extends AbstractServiceTest {
     @Test(expected = UserNotFoundException.class)
     public void testFailFindUserByEmail() throws Exception {
         userService.findByEmail("null");
+    }
+
+    @Before
+    public void setup() {
+//        userRepository = new UserRepository();
+    }
+
+    @Test
+    @Override
+    public void testCount() throws Exception {
+        User User = new User();
+        User.setEmail("Test User One");
+        userService.save(User);
+
+        User = new User();
+        User.setEmail("Test User Two");
+        userService.save(User);
+
+        User = new User();
+        User.setEmail("Test User Three");
+        userService.save(User);
+
+        assertThat("Should be three", userService.count(), is(3L));
+    }
+
+    @Test
+    @Override
+    public void testExists() throws Exception {
+        User User = new User();
+        User.setEmail("Existing User");
+        User = userService.save(User);
+        assertTrue("User should exist", userService.exists(User.getId()));
+    }
+
+    @Test
+    @Override
+    public void testFindOne() throws Exception {
+        User User = new User();
+        User.setEmail("Existing User");
+        User = userService.save(User);
+        assertNotNull("User should not be null", userService.findOne(User.getId()));
+    }
+
+    @Test
+    @Override
+    public void testSave() throws Exception {
+        User User = new User();
+        User.setEmail("Existing User");
+        assertNotNull("User should be saved", userService.save(User));
+    }
+
+    @Test
+    @Override
+    public void testSaveAll() throws Exception {
+        List<User> agencyList = new ArrayList<>();
+        User User = new User();
+        User.setEmail("Test User One");
+        agencyList.add(User);
+
+        User = new User();
+        User.setEmail("Test User Two");
+        agencyList.add(User);
+
+        User = new User();
+        User.setEmail("Test User Three");
+        agencyList.add(User);
+
+        userService.save(agencyList);
+
+        assertEquals("Should have saved 3 agencies", userService.count(), 3L);
+    }
+
+    @Test(expected = ResourceNotFoundException.class)
+    @Override
+    public void testDelete() throws Exception {
+        User User = new User();
+        User.setEmail("Existing User");
+        User = userService.save(User);
+        userService.delete(User.getId());
+
+        assertNull("Should return null", userService.findOne(User.getId()));
+    }
+
+    @Test(expected = ResourceNotFoundException.class)
+    @Override
+    public void testDeleteAll() throws Exception {
+        List<User> agencyList = new ArrayList<>();
+        User User = new User();
+        User.setEmail("Test User One");
+        agencyList.add(User);
+
+        User = new User();
+        User.setEmail("Test User Two");
+        agencyList.add(User);
+
+        User = new User();
+        User.setEmail("Test User Three");
+        agencyList.add(User);
+
+        agencyList = userService.save(agencyList);
+        userService.delete(agencyList);
+
+        agencyList.forEach(a -> assertNull("Should return null", userService.findOne(a.getId())));
+
     }
 }
