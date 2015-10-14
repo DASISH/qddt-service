32b13d308bd1dc9b25b284eb429db2a6e2d94e81
Dag Østgulen Heradstveit <dag.heradstveit@nsd.uib.no>
2015-10-14 13:22:24 +0200

    test

--- src/test/java/no/nsd/qddt/domain/instrumentquestion/InstrumentQuestionTest.java
+++ src/test/java/no/nsd/qddt/domain/instrumentquestion/InstrumentQuestionTest.java
@@ -1,75 +1,73 @@
-//package domain.instrumentquestion;
-//
-//import AbstractServiceTest;
-//import no.nsd.qddt.QDDT;
-//import no.nsd.qddt.domain.instrument.InstrumentService;
-//import no.nsd.qddt.domain.instrumentquestion.InstrumentQuestion;
-//import no.nsd.qddt.domain.instrumentquestion.InstrumentQuestionService;
-//import no.nsd.qddt.domain.question.QuestionService;
-//import no.nsd.qddt.no.nsd.qddt.utils.builders.InstrumentBuilder;
-//import no.nsd.qddt.no.nsd.qddt.utils.builders.InstrumentQuestionBuilder;
-//import no.nsd.qddt.no.nsd.qddt.utils.builders.QuestionBuilder;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.SpringApplicationConfiguration;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//
-//import java.util.List;
-//import java.util.UUID;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//public class InstrumentQuestionTest extends AbstractServiceTest {
-//
-//    @Autowired
-//    private QuestionService questionService;
-//
-//    @Autowired
-//    private InstrumentService instrumentService;
-//
-//    @Autowired
-//    private InstrumentQuestionService instrumentQuestionService;
-//
-//    @Before
-//    public void setUp() {
-//        //User user, String changeComment, String name
-//        instrumentService.save(new InstrumentBuilder().setName("Test Instrument 1").setChangeComment("Changed for tests.").createInstrument());
-//        instrumentService.save(new InstrumentBuilder().setName("Test Instrument 2").setChangeComment("Changed for more tests.").createInstrument());
-//
-//        questionService.save(new QuestionBuilder().setName("Male").setChangeComment("Changed typo").setInstructions("Used to find a man").createInstrument());
-//        questionService.save(new QuestionBuilder().setName("Female").setChangeComment("Changed changed another typo").setInstructions("Used to find a woman").createInstrument());
-//
-//        UUID qid1 = questionService.findAll().get(0).getId();
-//        UUID iid1 = instrumentService.findAll().get(0).getId();
-//
-//        instrumentQuestionService.save(new InstrumentQuestionBuilder().setName("A instrumentQuestion").setChangeComment("Changed because.")
-//                        .setQuestion(questionService.findOne(qid1)).setInstrument(instrumentService.findOne(iid1)).createInstrument());
-//
-//        UUID qid2 = questionService.findAll().get(1).getId();
-//        UUID iid2 = instrumentService.findAll().get(1).getId();
-//
-//        instrumentQuestionService.save(new InstrumentQuestionBuilder().setName("Different instrumentQuestion").setChangeComment("It had to be done at some point!")
-//                .setQuestion(questionService.findOne(qid2)).setInstrument(instrumentService.findOne(iid1)).createInstrument());
-//
-//        instrumentQuestionService.save(new InstrumentQuestionBuilder().setName("Totally different instrumentQuestion").setChangeComment("Again, something had to be done!")
-//                .setQuestion(questionService.findOne(qid2)).setInstrument(instrumentService.findOne(iid2)).createInstrument());
-//    }
-//
-//    @Test
-//    public void findByInstrumentTest() throws Exception {
-//        List<InstrumentQuestion> isq = instrumentQuestionService.findByInstrumentId(instrumentService.findAll().get(1).getId());
-//        assertEquals("Expected two elements!", isq.size(), 2);
-//    }
-//
-//    /**
-//     * Make sure only 1 of 3 possible questions are giving us htis here.
-//     * @throws Exception
-//     */
-//    @Test
-//    public void findByQuestionTest() throws Exception {
-//        List<InstrumentQuestion> iqs = instrumentQuestionService.findByQuestionId(questionService.findAll().get(2).getId());
-//        assertEquals("Expected two elements!", iqs.size(), 2);
-//    }
-//}
+package no.nsd.qddt.domain.instrumentquestion;
+
+import no.nsd.qddt.QDDT;
+import no.nsd.qddt.domain.instrument.Instrument;
+import no.nsd.qddt.domain.instrument.InstrumentService;
+import no.nsd.qddt.domain.question.Question;
+import no.nsd.qddt.domain.question.QuestionService;
+import no.nsd.qddt.utils.builders.InstrumentBuilder;
+import no.nsd.qddt.utils.builders.InstrumentQuestionBuilder;
+import no.nsd.qddt.utils.builders.QuestionBuilder;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = QDDT.class)
+public class InstrumentQuestionTest {
+
+    @Autowired
+    private QuestionService questionService;
+
+    @Autowired
+    private InstrumentService instrumentService;
+
+    @Autowired
+    private InstrumentQuestionService instrumentQuestionService;
+
+    private Instrument i1,i2;
+    private Question q1,q2;
+
+    @Before
+    public void setUp() {
+        i1 = instrumentService.save(new InstrumentBuilder().setName("Test Instrument 1").setChangeComment("Changed for tests.").createInstrument());
+        i2 = instrumentService.save(new InstrumentBuilder().setName("Test Instrument 2").setChangeComment("Changed for more tests.").createInstrument());
+
+        q1 = questionService.save(new QuestionBuilder().setName("Male").setChangeComment("Changed typo").setInstructions("Used to find a man").createInstrument());
+        q2 = questionService.save(new QuestionBuilder().setName("Female").setChangeComment("Changed changed another typo").setInstructions("Used to find a woman").createInstrument());
+
+        instrumentQuestionService.save(new InstrumentQuestionBuilder().setName("A instrumentQuestion").setChangeComment("Changed because.")
+                        .setQuestion(q1).setInstrument(i1).createInstrument());
+
+        instrumentQuestionService.save(new InstrumentQuestionBuilder().setName("Different instrumentQuestion").setChangeComment("It had to be done at some point!")
+                .setQuestion(q1).setInstrument(i2).createInstrument());
+
+        instrumentQuestionService.save(new InstrumentQuestionBuilder().setName("Totally different instrumentQuestion").setChangeComment("Again, something had to be done!")
+                .setQuestion(q2).setInstrument(i2).createInstrument());
+    }
+
+    @Test
+    public void findByInstrumentTest() throws Exception {
+        List<InstrumentQuestion> isq = instrumentQuestionService.findByInstrumentId(i1.getId());
+        assertEquals("Expected two elements!", isq.size(), 1);
+    }
+
+    /**
+     * Make sure only 1 of 3 possible questions are giving us htis here.
+     * @throws Exception
+     */
+    @Test
+    public void findByQuestionTest() throws Exception {
+        List<InstrumentQuestion> iqs = instrumentQuestionService.findByQuestionId(q1.getId());
+        assertEquals("Expected two elements!", iqs.size(), 2);
+    }
+}
