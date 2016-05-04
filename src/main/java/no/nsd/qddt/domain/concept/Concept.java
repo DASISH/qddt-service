25d07ec3c2dc472b9ec23c40cf6d53a64bbb6c34
SNO <stig.norland@nsd.uib.no>
2016-05-04 11:15:26 +0200

    errors was made, now back to normal.

--- src/main/java/no/nsd/qddt/domain/concept/Concept.java
+++ src/main/java/no/nsd/qddt/domain/concept/Concept.java
@@ -57,10 +57,10 @@ public class Concept extends AbstractEntityAudit implements Commentable, Authora
     @JoinColumn(name="topicgroup_id", updatable= false)
     private TopicGroup topicGroup;
 
-    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "questions")
-//    @JoinTable(name = "CONCEPT_QUESTION",
-//            joinColumns = {@JoinColumn(name ="concept_id", nullable = false, updatable = false)},
-//            inverseJoinColumns = {@JoinColumn(name = "question_id", nullable = false,updatable = false)})
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "CONCEPT_QUESTION",
+            joinColumns = {@JoinColumn(name ="concept_id", nullable = false, updatable = false)},
+            inverseJoinColumns = {@JoinColumn(name = "question_id", nullable = false,updatable = false)})
     private Set<Question> questions = new HashSet<>();
 
     @ManyToMany(fetch = FetchType.EAGER, mappedBy = "concepts", cascade = CascadeType.ALL)
