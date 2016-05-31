b6877bea246d32e3b7383a7d0f0fd1c70903515c
SNO <stig.norland@nsd.uib.no>
2016-05-31 08:03:27 +0200

    updated the names of authors

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -32,10 +32,10 @@ INSERT INTO user_authority (user_id, authority_id) VALUES('83d4c3ba-4ff9-11e5-88
 
 
 
-INSERT INTO author (id,  name, picture) VALUES ('83d4c30a-4ff9-11e5-885d-feff819cdc9f',  'Prof DrÃ¸vell', 'http://coraljs.com/example/contact/john-doe.jpeg');
-INSERT INTO author (id,  name, picture) VALUES ('83d4c30b-4ff9-11e5-885d-feff819cdc9f',  'Prof AI Head', 'http://www.secondpicture.com/tutorials/3d/human_head_reference_picture_front.jpg');
-INSERT INTO author (id,  name, picture) VALUES ('83d4c30c-4ff9-11e5-885d-feff819cdc9f',  'Dr Who', 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Matt_Smith_Cannes_2014.jpg/220px-Matt_Smith_Cannes_2014.jpg');
-INSERT INTO author (id,  name, picture) VALUES ('83d4c30d-4ff9-11e5-885d-feff819cdc9f',  'Professor Veldiglangnavn', 'http://coraljs.com/example/contact/john-doe.jpeg');
+INSERT INTO author (id,  name, picture) VALUES ('83d4c30a-4ff9-11e5-885d-feff819cdc9f',  'Prof Drøvell', 'http://coraljs.com/example/contact/john-doe.jpeg');
+INSERT INTO author (id,  name, picture) VALUES ('83d4c30b-4ff9-11e5-885d-feff819cdc9f',  'Professor AI Head', 'http://www.secondpicture.com/tutorials/3d/human_head_reference_picture_front.jpg');
+INSERT INTO author (id,  name, picture) VALUES ('83d4c30c-4ff9-11e5-885d-feff819cdc9f',  'Doctor Who', 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Matt_Smith_Cannes_2014.jpg/220px-Matt_Smith_Cannes_2014.jpg');
+INSERT INTO author (id,  name, picture) VALUES ('83d4c30d-4ff9-11e5-885d-feff819cdc9f',  'Professor Veldiglangtnavn', 'http://coraljs.com/example/contact/john-doe.jpeg');
 UPDATE author SET  email = 'test@example.org', homepage = 'http://www.nsd.no' WHERE id = '83d4c30a-4ff9-11e5-885d-feff819cdc9f';
 UPDATE author SET  homepage = 'http://ai.uni-bremen.de/team/michael_beetz' WHERE id = '83d4c30b-4ff9-11e5-885d-feff819cdc9f';
 UPDATE author SET   homepage = 'https://en.wikipedia.org/wiki/Doctor_Who' WHERE id = '83d4c30c-4ff9-11e5-885d-feff819cdc9f';
