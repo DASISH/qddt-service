23164c069e081d91af396c576e11d05f1b4f778a
SNO <stig.norland@nsd.uib.no>
2016-06-08 10:26:15 +0200

    added more senseable data for categories

---
+++ src/main/resources/qddt-test-brukere.sql
@@ -0,0 +1,28 @@
+INSERT INTO user_account(id, username, password, email, agency_id) 
+VALUES('83d4c3bb-4ff9-11e5-885d-feff819cdc9f', 'Hilde', '$2a$10$O1MMi3SLcvwtJIT9CSZyN.aLtFKN.K2LtKyHZ52wElo0zh5gI1EyW', 'hilde.orten@nsd.no', '1359dede-9f18-11e5-8994-feff819cdc9f');
+
+INSERT INTO user_account(id, username, password, email, agency_id) 
+VALUES('83d4c3bc-4ff9-11e5-885d-feff819cdc9f', 'Knut', '$2a$10$O1MMi3SLcvwtJIT9CSZyN.aLtFKN.K2LtKyHZ52wElo0zh5gI1EyW', 'knut.skjak@nsd.no', '1359dede-9f18-11e5-8994-feff819cdc9f');
+
+INSERT INTO user_account(id, username, password, email, agency_id) 
+VALUES('83d4c3bd-4ff9-11e5-885d-feff819cdc9f', 'Sarah Butt', '$2a$10$O1MMi3SLcvwtJIT9CSZyN.aLtFKN.K2LtKyHZ52wElo0zh5gI1EyW', 'sarah.butt.1@city.ac.uk', '1359dede-9f18-11e5-8994-feff819cdc9f');
+
+INSERT INTO user_account(id, username, password, email, agency_id) 
+VALUES('83d4c3be-4ff9-11e5-885d-feff819cdc9f', 'Lizzy Winstone', '$2a$10$O1MMi3SLcvwtJIT9CSZyN.aLtFKN.K2LtKyHZ52wElo0zh5gI1EyW', 'lizzy.winstone.1@city.ac.uk', '1359dede-9f18-11e5-8994-feff819cdc9f');
+
+INSERT INTO user_account(id, username, password, email, agency_id) 
+VALUES('83d4c3c1-4ff9-11e5-885d-feff819cdc9f', 'ESS', '$2a$10$O1MMi3SLcvwtJIT9CSZyN.aLtFKN.K2LtKyHZ52wElo0zh5gI1EyW', 'ess@city.ac.uk', '1359dede-9f18-11e5-8994-feff819cdc9f');
+
+INSERT INTO user_account(id, username, password, email, agency_id) 
+VALUES('83d4c3c2-4ff9-11e5-885d-feff819cdc9f', 'Benjamin', '$2a$10$O1MMi3SLcvwtJIT9CSZyN.aLtFKN.K2LtKyHZ52wElo0zh5gI1EyW', 'benjamin.beuster@nsd.no', '1359dede-9f18-11e5-8994-feff819cdc9f');
+
+
+
+
+
+INSERT INTO user_authority (user_id, authority_id) VALUES('83d4c3bb-4ff9-11e5-885d-feff819cdc9f', '9bec2d6a-4ff9-11e5-885d-feff819cdc9f');
+INSERT INTO user_authority (user_id, authority_id) VALUES('83d4c3bc-4ff9-11e5-885d-feff819cdc9f', '9bec2d6a-4ff9-11e5-885d-feff819cdc9f');
+INSERT INTO user_authority (user_id, authority_id) VALUES('83d4c3bd-4ff9-11e5-885d-feff819cdc9f', '9bec2d6a-4ff9-11e5-885d-feff819cdc9f');
+INSERT INTO user_authority (user_id, authority_id) VALUES('83d4c3be-4ff9-11e5-885d-feff819cdc9f', '9bec2d6a-4ff9-11e5-885d-feff819cdc9f');
+INSERT INTO user_authority (user_id, authority_id) VALUES('83d4c3c1-4ff9-11e5-885d-feff819cdc9f', '9bec2d6a-4ff9-11e5-885d-feff819cdc9f');
+INSERT INTO user_authority (user_id, authority_id) VALUES('83d4c3c2-4ff9-11e5-885d-feff819cdc9f', '9bec2d6a-4ff9-11e5-885d-feff819cdc9f');
