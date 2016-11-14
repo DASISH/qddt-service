96672dec23e64c2fa11b7e1d783ae6f737a79762
Thomas Amland <thomas.amland@nsd.no>
2016-11-14 13:23:10 +0100

    fix security config

--- src/main/java/no/nsd/qddt/configuration/SecurityConfiguration.java
+++ src/main/java/no/nsd/qddt/configuration/SecurityConfiguration.java
@@ -4,32 +4,30 @@ import no.nsd.qddt.domain.user.QDDTUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 /**
  * @author Dag Østgulen Heradstveit
  */
 @Configuration
 @EnableWebSecurity
-@EnableWebMvcSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@EnableResourceServer
+public class SecurityConfiguration extends ResourceServerConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        // Disabled CSRF for now.
-        // INFO: http://en.wikipedia.org/wiki/Cross-site_request_forgery
-        http
-                .csrf().disable();
-
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
         http.authorizeRequests()
-                .antMatchers("/**").hasAnyRole("USER", "ADMIN");
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .antMatchers("/**").authenticated();
     }
 
     /**
