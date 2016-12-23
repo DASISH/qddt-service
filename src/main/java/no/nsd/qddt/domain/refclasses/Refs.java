cbab02ec7c8955841d6a6973946f9867f0df955f
StigNorland <Nukegithub01>
2016-12-23 09:39:27 +0100

    added description

--- src/main/java/no/nsd/qddt/domain/refclasses/Refs.java
+++ src/main/java/no/nsd/qddt/domain/refclasses/Refs.java
@@ -4,6 +4,8 @@ import java.util.UUID;
 
 /**
  * @author Stig Norland
+ * A ref is a simple interface which is intended to help reporting backreferences
+ * without ending up with a circular reference loop.
  */
 public interface Refs {
     String getName();
