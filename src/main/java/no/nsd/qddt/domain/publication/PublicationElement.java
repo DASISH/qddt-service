c77933d3e6eb5e87099a595cd9e848053b8aa1a6
StigNorland <Nukegithub01>
2017-01-18 15:39:22 +0100

    https://github.com/DASISH/qddt-client/issues/289
    issue # 289

--- src/main/java/no/nsd/qddt/domain/publication/PublicationElement.java
+++ src/main/java/no/nsd/qddt/domain/publication/PublicationElement.java
@@ -1,10 +1,22 @@
 package no.nsd.qddt.domain.publication;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import no.nsd.qddt.domain.AbstractEntityAudit;
+import no.nsd.qddt.domain.concept.Concept;
+import no.nsd.qddt.domain.controlconstruct.ControlConstruct;
+import no.nsd.qddt.domain.instrument.Instrument;
+import no.nsd.qddt.domain.questionItem.QuestionItem;
+import no.nsd.qddt.domain.study.Study;
+import no.nsd.qddt.domain.surveyprogram.SurveyProgram;
+import no.nsd.qddt.domain.topicgroup.TopicGroup;
 import org.hibernate.annotations.Type;
 import org.hibernate.envers.Audited;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.persistence.Transient;
 import java.util.UUID;
+
 
 /**
  * @author Stig Norland
@@ -19,6 +31,10 @@ public class PublicationElement  {
     private Integer revisionNumber;
 
     private ElementKind elementKind;
+
+    @Transient
+    @JsonSerialize
+    private String element;
 
 
     public PublicationElement() {
@@ -46,6 +62,53 @@ public class PublicationElement  {
 
     public void setElementKind(ElementKind elementKind) {
         this.elementKind = elementKind;
+    }
+
+    public String getElement() {
+        return element;
+    }
+
+    public void setElement(Concept element) {
+        System.out.println(element);
+        this.element = element.toString();
+//        Gson gson = new Gson();
+//        this.element =  gson.toJson(element);
+    }
+    public void setElement(ControlConstruct element) {
+        System.out.println(element);
+        this.element = element.toString();
+//        Gson gson = new Gson();
+//        this.element =  gson.toJson(element);
+    }
+    public void setElement(Instrument element) {
+        System.out.println(element);
+        this.element = element.toString();
+//        Gson gson = new Gson();
+//        this.element =  gson.toJson(element);
+    }
+    public void setElement(QuestionItem element) {
+        System.out.println(element);
+        this.element = element.toString();
+//        Gson gson = new Gson();
+//        this.element =  gson.toJson(element);
+    }
+    public void setElement(Study element) {
+        System.out.println(element);
+        this.element = element.toString();
+//        Gson gson = new Gson();
+//        this.element =  gson.toJson(element);
+    }
+    public void setElement(SurveyProgram element) {
+        System.out.println(element);
+        this.element = element.toString();
+//        Gson gson = new Gson();
+//        this.element =  gson.toJson(element);
+    }
+    public void setElement(TopicGroup element) {
+        System.out.println(element);
+        this.element = element.toString();
+//        Gson gson = new Gson();
+//        this.element =  gson.toJson(element);
     }
 
     @Override
--- src/main/java/no/nsd/qddt/domain/publication/PublicationServiceImpl.java
+++ src/main/java/no/nsd/qddt/domain/publication/PublicationServiceImpl.java
@@ -1,5 +1,12 @@
 package no.nsd.qddt.domain.publication;
 
+import no.nsd.qddt.domain.concept.audit.ConceptAuditService;
+import no.nsd.qddt.domain.controlconstruct.audit.ControlConstructAuditService;
+import no.nsd.qddt.domain.instrument.audit.InstrumentAuditService;
+import no.nsd.qddt.domain.questionItem.audit.QuestionItemAuditService;
+import no.nsd.qddt.domain.study.audit.StudyAuditService;
+import no.nsd.qddt.domain.surveyprogram.audit.SurveyProgramAuditService;
+import no.nsd.qddt.domain.topicgroup.audit.TopicGroupAuditService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,10 +22,32 @@ import java.util.UUID;
 public class PublicationServiceImpl implements PublicationService {
 
     private PublicationRepository repository;
+    private ConceptAuditService conceptService;
+    private ControlConstructAuditService controlConstructService;
+    private InstrumentAuditService instrumentService;
+    private QuestionItemAuditService questionItemService;
+    private StudyAuditService studyService;
+    private SurveyProgramAuditService surveyProgramService;
+    private TopicGroupAuditService topicGroupService;
 
     @Autowired
-    public PublicationServiceImpl(PublicationRepository repository) {
+    public PublicationServiceImpl(PublicationRepository repository,
+                                  ConceptAuditService conceptService,
+                                  ControlConstructAuditService controlConstructService,
+                                  InstrumentAuditService instrumentService,
+                                  QuestionItemAuditService questionItemService,
+                                  StudyAuditService studyService,
+                                  SurveyProgramAuditService surveyProgramService,
+                                  TopicGroupAuditService topicGroupService)
+        {
         this.repository = repository;
+        this.conceptService = conceptService;
+        this.controlConstructService = controlConstructService;
+        this.instrumentService = instrumentService;
+        this.questionItemService = questionItemService;
+        this.studyService = studyService;
+        this.surveyProgramService = surveyProgramService;
+        this.topicGroupService = topicGroupService;
     }
 
     @Override
@@ -33,7 +62,7 @@ public class PublicationServiceImpl implements PublicationService {
 
     @Override
     public Publication findOne(UUID uuid) {
-        return repository.findOne(uuid);
+        return fillElements(repository.findOne(uuid));
     }
 
     @Override
@@ -58,6 +87,39 @@ public class PublicationServiceImpl implements PublicationService {
 
     @Override
     public Page<Publication> findAllPageable(Pageable pageable) {
-        return repository.findAll(pageable);
+        return repository.findAll(pageable).map(p->fillElements(p));
     }
+
+    private Publication fillElements(Publication publication){
+        publication.getPublicationElements().stream().map(e->fill(e));
+        return publication;
+    }
+
+    private PublicationElement fill(PublicationElement element){
+        switch (element.getElementKind()) {
+            case CONCEPT:
+                element.setElement(conceptService.findRevision(element.getId(),element.getRevisionNumber()).getEntity());
+                break;
+            case CONTROL_CONSTRUCT:
+                element.setElement(controlConstructService.findRevision(element.getId(),element.getRevisionNumber()).getEntity());
+                break;
+            case INSTRUMENT:
+                element.setElement(instrumentService.findRevision(element.getId(),element.getRevisionNumber()).getEntity());
+                break;
+            case QUESTION_ITEM:
+                element.setElement(questionItemService.findRevision(element.getId(),element.getRevisionNumber()).getEntity());
+                break;
+            case STUDY:
+                element.setElement(studyService.findRevision(element.getId(),element.getRevisionNumber()).getEntity());
+                break;
+            case SURVEY_PROGRAM:
+                element.setElement(surveyProgramService.findRevision(element.getId(),element.getRevisionNumber()).getEntity());
+                break;
+            case TOPIC_GROUP:
+                element.setElement(topicGroupService.findRevision(element.getId(),element.getRevisionNumber()).getEntity());
+                break;
+        }
+        return element;
+    }
+
 }
