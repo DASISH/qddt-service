9b8d9e3cb899db95142bd9e01ff84586aa3dc3a5
Stig Norland <stig.norland@gmail.com>
2018-02-21 13:40:04 +0100

    LOG bugs introduced... fixed

--- src/main/java/no/nsd/qddt/domain/category/Category.java
+++ src/main/java/no/nsd/qddt/domain/category/Category.java
@@ -413,12 +413,12 @@ public class Category extends AbstractEntityAudit  implements Comparable<Categor
         }
     }
 
-    @Override
-    public void makeNewCopy(Long revision){
-        // Copying a simple Category doesn't make any sense... skipping...
-        hasRun = (getHierarchyLevel() == HierarchyLevel.ENTITY);
-        if (hasRun) return;
-        super.makeNewCopy(revision);
-        getChildren().forEach(c->c.makeNewCopy(revision));
-    }
+//    @Override
+//    public void makeNewCopy(Long revision){
+//        // Copying a simple Category doesn't make any sense... skipping...
+//        hasRun = (getHierarchyLevel() == HierarchyLevel.ENTITY);
+//        if (hasRun) return;
+//        super.makeNewCopy(revision);
+//        getChildren().forEach(c->c.makeNewCopy(revision));
+//    }
 }
