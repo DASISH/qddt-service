5d9b4830d2eb11cc50e2c8f69e122ab7e19b3d36
Stig Norland <stig.norland@gmail.com>
2018-04-16 15:19:02 +0200

    search from revisions, not current content...

--- src/main/java/no/nsd/qddt/domain/questionItem/QuestionItemRepository.java
+++ src/main/java/no/nsd/qddt/domain/questionItem/QuestionItemRepository.java
@@ -18,11 +18,11 @@ public interface QuestionItemRepository extends BaseRepository<QuestionItem,UUID
     Page<QuestionItem> findAll(Pageable pageable);
 
     @Query(value = "SELECT qi.* FROM QUESTION_ITEM qi " +
-        "LEFT JOIN responsedomain r on qi.responsedomain_id = r.id " +
+        "LEFT JOIN audit.responsedomain_aud r on qi.responsedomain_id = r.id and qi.responsedomain_revision = r.rev " +
         "WHERE ( qi.name ILIKE :name or qi.question ILIKE :question or r.name ILIKE  :responseDomain ) "
         + "ORDER BY ?#{#pageable}"
         ,countQuery = "SELECT count(qi.*) FROM QUESTION_ITEM qi " +
-        "LEFT JOIN responsedomain r on qi.responsedomain_id = r.id " +
+        "LEFT JOIN audit.responsedomain_aud r on qi.responsedomain_id = r.id and qi.responsedomain_revision = r.rev " +
         "WHERE ( qi.name ILIKE :name or qi.question ILIKE :question or r.name ILIKE  :responseDomain ) "
         ,nativeQuery = true)
     Page<QuestionItem> findByQuery(@Param("name")String name,
