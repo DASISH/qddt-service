8b899a840d1b58408a93ecebc1e9be86eafff1e4
Stig Norland <stig.norland@nsd.no>
2020-12-05 13:57:30 +0100

    tsetes

--- src/main/java/no/nsd/qddt/domain/questionitem/audit/QuestionItemAuditRepository.java
+++ src/main/java/no/nsd/qddt/domain/questionitem/audit/QuestionItemAuditRepository.java
@@ -1,7 +1,7 @@
-package no.nsd.qddt.domain.questionitem.audit;
+package no.nsd.qddt.domain.questionItem.audit;
 
 import no.nsd.qddt.domain.AbstractEntityAudit;
-import no.nsd.qddt.domain.questionitem.QuestionItem;
+import no.nsd.qddt.domain.questionItem.QuestionItem;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.history.Revision;
